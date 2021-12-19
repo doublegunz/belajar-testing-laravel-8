6de8518a0a1ee1967fe1eca371313315fc72e20e
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-12-19 16:54:17 +0700

    add bootstrap ui

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,26 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tasks Management</title>
-</head>
-<body>
-    <form action="{{ route('tasks.store') }}" method="post">
-        @csrf
-        <input type="text" name="name" id="name">
-        <textarea name="description" id="" cols="30" rows="10"></textarea>
-        <input type="submit" value="Create Task">
-    </form>
-    <h1>Tasks Management</h1>
-    <ul>
-        @foreach ($tasks as $task)
-        <li>
-            {{ $task->name }} <br>
-            {{ $task->description }}
-        </li>
-        @endforeach
-    </ul>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<h1 class="page-header text-center">Tasks Management</h1>
+<div class="row justify-content-center">
+    <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card">
+            <div class="card-body">
+                <h2>Tasks</h2>
+                <ul class="list-group list-group-flush">
+                    @foreach ($tasks as $task)
+                    <li class="list-group-item">
+                        {{ $task->name }}<br>
+                        {{ $task->description }}
+                    </li>
+                    @endforeach
+                </ul>
+
+            </div>
+        </div>
+
+
+
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card">
+            <div class="card-body">
+                <h2>New Task</h2>
+                <form action="{{ route('tasks.index') }}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="exampleInputname1" class="form-label">Nama</label>
+                        <input type="text" class="form-control" name="name" aria-describedby="nameHelp">
+                        <div id="nameHelp" class="form-text">We'll never share your name with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Description</label>
+                        <textarea name="description" id="" cols="30" rows="10" class="form-control"></textarea>
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Create Task</button>
+                </form>
+
+            </div>
+        </div>
+
+
+    </div>
+</div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 
 Route::get('tasks', [TasksController::class, 'index'])->name('tasks.index');
 Route::post('tasks', [TasksController::class, 'store'])->name('tasks.store');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
